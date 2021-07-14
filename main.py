af91f84eaef56eea0d8f0a5185d0be0a7be20dee
lenguyen1605 <nguyennhatle1605@gmail.com>
2021-07-14 11:18:11 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+import os
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('integers', type=int)
+    args = parser.parse_args()
+    directory = "CatProject"
+
+    parent_dir = '/Users/lenguyen/Desktop'
+    path = os.path.join(parent_dir, directory)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    for i in range(args.integers):
+        location = os.path.join(path, 'cat' + str(i))
+
+        r = requests.get('https://cataas.com/cat')
+        with open(location, 'wb') as f:
+            f.write(r.content)
+
+    print(r.headers.get('content-type'))
+
+main()
